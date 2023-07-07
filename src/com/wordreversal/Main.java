da9d4fb8cd0164ddcc176fad98f628b9e4694ef0
guilherme.frank <contatoguilhermefrank@gmail.com>
2023-07-07 11:11:19 -0300

    final changes

--- src/com/wordreversal/Main.java
+++ src/com/wordreversal/Main.java
@@ -1,2 +1,26 @@
-package com.wordreversal;public class Main {
+package com.wordreversal;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Type the word that will be reversed: ");
+        String word = input.nextLine();
+
+        System.out.println("Word (Right Order): " + word);
+        System.out.println("Word (Reversed): " + reverseWord(word));
+
+
+    }
+
+    public static StringBuilder reverseWord(String word){
+        StringBuilder reversedWord = new StringBuilder(word);
+
+        return reversedWord.reverse();
+    }
+
+
 }
